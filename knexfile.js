3b534143b8964330832ab8d2862adea6ba110676
iner Lofgren <iner.lofgren@gmail.com>
2019-05-30 19:30:03 -0700

    mvp

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/recipeBook.db3',
+      filename: "./data/recipies.sqlite3"
     },
     useNullAsDefault: true, // needed for sqlite
-  },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
